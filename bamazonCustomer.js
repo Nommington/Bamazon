11f0021ef84ca465dc409c98f7947612a9c35f47
Nommington <rymckenz@gmail.com>
2018-06-11 22:12:30 -0400

    complete, working customer interface

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,8 @@ var inquirer = require("inquirer");
 var Table = require("cli-table");
 require("dotenv").config();
 idArr = [];
-
+var item = "";
+var quan = 0;
 var table = new Table({
     chars: { 'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗', 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝', 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼', 'right': '║', 'right-mid': '╢', 'middle': '│' }
 });
@@ -29,7 +30,7 @@ function dispTable(res) {
         var id = res[i].item_id;
         var name = res[i].product_name;
         var dept = res[i].department_name;
-        var price = res[i].price;
+        var price = res[i].price.toFixed(2);
         var stock = res[i].stock_quantity;
         arr = [id, name, dept, "$" + price, stock];
         table.push(arr);
@@ -37,16 +38,47 @@ function dispTable(res) {
     console.log(table.toString());
 }
 
-function prompt(res) {
+function inventoryChange(id, quan) {
 
+    connection.query("UPDATE products SET stock_quantity = " + quan + " WHERE item_id = " + id, function (err, results) {
+        if (err) throw err;
+    });
 }
+
+function receipt(res) {
+    console.log("ORDER CONFIRMATION");
+    console.log("--------------------------------------------");
+    console.log("ITEM: " + item.product_name);
+    console.log("PRICE: $" + item.price);
+    console.log("QUANTITY: " + quan);
+    console.log("TOTAL: $" + (item.price * quan).toFixed(2));
+}
+
+function finishUp(quan, item) {
+                        newQuan = parseInt(item.stock_quantity) - quan;
+                        inventoryChange(item.item_id, newQuan);
+                        receipt();
+                        inquirer.prompt([{
+                            type: "confirm",
+                            name: "again",
+                            message: "Would you like to continue shopping?"
+                        }]).then(function (answ) {
+                            if (answ.again) {
+                                marketplace();
+                            }
+                            else {
+                                console.log("Thank you for using BAMAZON!");
+                                process.exit();
+                            }
+                        });
+                    }
 
 function marketplace() {
 
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
         dispTable(results);
-        
+
         console.log("WELCOME TO BAMAZON!");
         inquirer.prompt([
             {
@@ -54,7 +86,7 @@ function marketplace() {
                 name: "choice",
                 message: "Please enter the ITEM ID of the item you'd like to purchase.",
                 validate: function (input) {
-                    if (parseInt(input) <= 0 || parseInt(input) > results.length) {
+                    if (parseInt(input) <= 0 || parseInt(input) > results.length || isNaN(parseInt(input))) {
                         console.log("\nPlease make sure to enter a VALID ITEM ID.");
                         return false;
                     }
@@ -76,19 +108,35 @@ function marketplace() {
                 }
             }
         ]).then(function (answer) {
-            var item = results[parseInt(answer.choice) - 1];            
-            var quantity = parseInt(answer.quantity);
-            if (quantity > item.stock_quantity) {
-                console.log("I'm sorry. We only have " + item.stock_quantity + "of those in stock.");
-                //prompt
+            item = results[parseInt(answer.choice) - 1];
+            quan = parseInt(answer.quantity);
+            if (quan > item.stock_quantity) {
+                console.log("\nI'm sorry. We only have " + item.stock_quantity + " of those in stock.");
+                inquirer.prompt([{
+                    type: "input",
+                    name: "quant",
+                    message: "How many " + item.product_name + " would you like?",
+                    validate: function (input) {
+                        if (parseInt(input) <= item.stock_quantity) {
+                            return true;
+                        }
+                        else {
+                            console.log(input.quant);
+                            console.log("\nI'm sorry. We only have " + item.stock_quantity + " of those in stock.");
+                            return false;
+                        }
+                    }
+                }]).then(function (ans) {
+                    quan = parseInt(ans.quant);
+                    finishUp(quan, item);
+                    
+                });
+
+
+
             }
             else {
-                console.log("ORDER CONFIRMATION");
-                console.log("--------------------------------------------");
-                console.log("ITEM: " + item.product_name);
-                console.log("PRICE: $" + item.price);
-                console.log("QUANTITY: " + quantity);
-                console.log("TOTAL: $" + (item.price * quantity).toFixed(2));
+                finishUp(quan, item);
             }
         });
 
